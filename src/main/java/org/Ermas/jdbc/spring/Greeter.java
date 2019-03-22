8123d3e7647176f5c1243345aeea3b469b5f1b89
Ermas <hys0705@gmail.com>
2019-03-22 11:55:42 +0900

    2019-03-22

---
+++ src/main/java/org/Ermas/jdbc/spring/Greeter.java
@@ -0,0 +1,11 @@
+package org.Ermas.jdbc.spring;
+
+public class Greeter {
+	public Greeter() {
+		System.out.println("Greeter instance create!");
+	}
+	
+	public String greet() {
+		return "Hello, World!";
+	}
+}
---
+++ src/main/java/org/Ermas/jdbc/spring/NomalMain.java
@@ -0,0 +1,8 @@
+package org.Ermas.jdbc.spring;
+
+public class NomalMain {
+	public static void main(String[] args) {
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.greet());
+	}
+}
